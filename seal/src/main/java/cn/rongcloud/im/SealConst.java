7ffb0498e2702f801233b8241ec26b95103dddfc
阿明 <13120241790@163.com>
2016-09-28 11:48:11 +0800

    Update sdk v2.7.3 stable

--- seal/src/main/java/cn/rongcloud/im/SealConst.java
+++ seal/src/main/java/cn/rongcloud/im/SealConst.java
@@ -12,6 +12,6 @@ public class SealConst {
     public static final int PERSONALPROFILEFROMGROUP = 4;
     public static final String EXIT = "EXIT";
     public static final String CHANGEINFO = "CHANGEINFO";
-    public static final String SEALTALKVERSION = "1.1.3";
+    public static final String SEALTALKVERSION = "1.1.4";
 
 }
