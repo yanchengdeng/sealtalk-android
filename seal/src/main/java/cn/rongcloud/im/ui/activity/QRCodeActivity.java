81603acd8de8fc5fb569b6132c748f80c57b0d30
star1209 <star5603@163.com>
2018-05-07 11:12:47 +0800

    1、换肤、换icon及标题； 2、我的页面

---
+++ seal/src/main/java/cn/rongcloud/im/ui/activity/QRCodeActivity.java
@@ -0,0 +1,36 @@
+package cn.rongcloud.im.ui.activity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import cn.rongcloud.im.R;
+import cn.rongcloud.im.server.utils.QRUtils;
+
+/**
+ * Created by star1209 on 2018/5/6.
+ */
+
+public class QRCodeActivity extends BaseActivity {
+
+    private ImageView mIvQrCode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+        setHeadRightButtonVisibility(View.GONE);
+        setTitle(R.string.baojia_qrcode_title, false);
+
+        mIvQrCode = findViewById(R.id.iv_qrcode);
+        setQRCode();
+    }
+
+    private void setQRCode() {
+        int width = getResources().getDisplayMetrics().widthPixels - 120;
+        int height = width;
+        Bitmap qrCodeBitmap = QRUtils.createQRImage("ddd", width, height);
+        mIvQrCode.setImageBitmap(qrCodeBitmap);
+    }
+}
